e846ac241a4ed4555e36375a562b778d2f42edb4
Elena Ilieska <git@github.com:Ilieska/IS.git>
2024-02-18 22:26:33 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import mognoose from 'mongoose';
+import dotenv from 'dotenv'
 
-
+dotenv.config();
+mognoose.connect(process.env.MONGO).then(()=>{
+    console.log('conected to mongo db');
+}).catch((err)=>{
+    console.log(err);
+});
 const app = express();
 
 app.listen(3000, () => {
