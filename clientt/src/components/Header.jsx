3b0455a66e611378152a2bd57435cf535e611547
Elena Ilieska <git@github.com:Ilieska/IS.git>
2024-02-20 22:36:32 +0100

    complete sign up UI

--- clientt/src/components/Header.jsx
+++ clientt/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='header shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -17,17 +17,17 @@ export default function Header() {
             </form>
             <ul className='flex gap-5'>
                 <Link to = '/'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>
+                    <li className='hidden sm:inline text-slate-900 hover:underline'>
                         Home
                     </li>
                 </Link>
                 <Link to='/about'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>
+                    <li className='hidden sm:inline text-slate-900 hover:underline'>
                         About
                     </li>
                 </Link>
                 <Link to='/sign-in'>
-                    <li className=' text-slate-700 hover:underline'>
+                    <li className=' text-slate-900 hover:underline'>
                         Sign in
                     </li>
                 </Link>
