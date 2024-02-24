c01b4d0159223ed2e4807a8abb4b133cb868cba1
Elena Ilieska <git@github.com:Ilieska/IS.git>
2024-02-24 17:06:06 +0100

    update the header and make the profile page private

--- clientt/src/App.jsx
+++ clientt/src/App.jsx
@@ -6,18 +6,21 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
-  return (
-  <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-  </BrowserRouter>
-);
+    return (
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route  element={<PrivateRoute />}> 
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
+  );
 }
